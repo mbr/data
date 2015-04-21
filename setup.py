4ecbc39eca0425992ffe586341d33383fb681cf5
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-21 13:57:47 +0200

    Add the usual setup/README scaffolding.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='data',
+    version='0.1.dev1',
+    description='Work with unicode/non-unicode data from files or strings '
+                'uniformly.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/data',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['six'],
+)
