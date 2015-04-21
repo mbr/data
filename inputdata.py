6fe8fec53182930273330e3dc1cfc210e1b19669
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-21 13:55:55 +0200

    Initial import.

---
+++ inputdata.py
@@ -0,0 +1,98 @@
+from six import text_type, PY2
+
+
+class InputData(object):
+    data = None
+    text = None
+    file = None
+    filename = None
+
+    def __init__(self, arg=None, encoding=None, data=None, file=None):
+        self.orig_args = (arg, data, file, encoding)
+        if [arg, data, file].count(None) != 2:
+            raise ValueError('Must supply exactly one of data or file')
+
+        # when given a positional argument, try to be smart
+        if arg is not None:
+            if hasattr(arg, 'read'):
+                file = arg
+            else:
+                data = arg
+            arg = None
+
+        if data is not None:
+            if isinstance(data, text_type):
+                self.text = data
+            else:
+                self.data = data
+        elif file is not None:
+            if hasattr(file, 'read'):
+                self.file = file
+                if getattr(file, 'encoding', None):
+                    encoding = file.encoding
+            else:
+                self.filename = file
+
+        self.encoding = encoding or 'utf8'
+
+    def __bytes__(self):
+        if self.data is not None:
+            return self.data
+
+        if self.text is not None:
+            return self.text.encode(self.encoding)
+
+        if self.file is not None:
+            if getattr(self.file, 'encoding', None):
+                # text is open in text mode
+                return self.file.read().encode(self.file.encoding)
+            return self.file.read()
+
+        if self.filename is not None:
+            with open(self.filename, 'rb') as f:
+                return f.read()
+
+        raise ValueError('Broken InputData, all None.')
+
+    def __str__(self):
+        if PY2:
+            return self.__bytes__()
+        return self.__unicode__()
+
+    def __unicode__(self):
+        if self.text is not None:
+            return self.text
+
+        if self.file is not None and getattr(self.file, 'encoding', None):
+            return self.file.read()
+
+        return self.__bytes__().decode(self.encoding)
+
+    def __repr__(self):
+        def head(buf):
+            if len(buf) < 20:
+                return repr(buf)
+
+            return repr(buf[:20]) + '...'
+
+        cname = self.__class__.__name__
+
+        if self.data is not None:
+            return '{}(data={}, encoding={!r})'.format(
+                cname, head(self.data), self.encoding,
+            )
+
+        if self.text is not None:
+            return '{}(data={}, encoding={!r]})'.format(
+                cname, head(self.text), self.encoding,
+            )
+
+        return '{}(file={!r}, encoding={!r})'.format(
+            cname, self.file or self.filename, self.encoding,
+        )
+
+    def read(self):
+        pass
+
+    def readb(self):
+        pass
---
+++ tests/test_inputdata.py
@@ -0,0 +1,87 @@
+# coding: utf-8
+
+import os
+import tempfile
+
+from six import text_type, binary_type, PY2
+import pytest
+
+from data.inputdata import InputData as I
+
+
+@pytest.fixture(
+    params=[u'This is sample data as unicode. äüöå \\',
+            u'',
+            u'\0']
+)
+def val(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=['utf8', 'latin1']
+)
+def encoding(request):
+    return request.param
+
+
+@pytest.yield_fixture
+def tmpfile():
+    tmpfd, tmpfn = tempfile.mkstemp()
+    try:
+        yield tmpfn
+    finally:
+        os.unlink(tmpfn)
+
+
+@pytest.fixture
+def valfile(val, tmpfile, encoding):
+    with open(tmpfile, 'w') as f:
+        f.write(val.encode(encoding))
+    return tmpfile
+
+
+@pytest.fixture(
+    params=['file', 'filename', 'unicode', 'string', 'smart_file',
+            'smart_unicode', 'smart_string',
+            pytest.mark.skipif("PY2")('file_no_encoding')]
+)
+def d(val, request, encoding, valfile):
+    if request.param == 'file':
+        v = I(file=open(valfile, 'rb'), encoding=encoding)
+    elif request.param == 'filename':
+        v = I(file=valfile, encoding=encoding)
+    elif request.param == 'unicode':
+        v = I(data=val, encoding=encoding)
+    elif request.param == 'string':
+        v = I(data=val.encode(encoding), encoding=encoding)
+    elif request.param == 'smart_file':
+        v = I(open(valfile, 'rb'), encoding=encoding)
+    elif request.param == 'smart_unicode':
+        v = I(val, encoding=encoding)
+    elif request.param == 'smart_string':
+        v = I(val.encode(encoding), encoding=encoding)
+    elif request.param == 'file_no_encoding':
+        v = I(open(valfile, 'r', encoding=encoding))
+    else:
+        raise RuntimeError
+
+    v._test_req = request.param
+
+    return v
+
+
+def test_getting_contents_as_bytes_str(d, val, encoding):
+    assert binary_type(d) == val.encode(encoding)
+
+
+def test_getting_contents_as_unicode(d, val):
+    assert text_type(d) == val
+
+
+def test_getting_contents_via_read(d, val):
+    assert d.read() == val
+
+
+def test_getting_contents_via_binary_read(d, val, encoding):
+    assert d.readb() == val.encode(encoding)
