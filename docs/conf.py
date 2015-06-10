d8cb0d36b76f7465d2bc0086c68946e5f083aa67
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-11 00:43:53 +0200

    Added nitpick_ignore for file.read

--- docs/conf.py
+++ docs/conf.py
@@ -244,3 +244,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# broken references in python docs
+nitpick_ignore = [('py:func', 'file.read')]
